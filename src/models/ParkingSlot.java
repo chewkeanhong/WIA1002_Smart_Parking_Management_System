e9a2da12b5e84d75e4984753341a9c66b3dca3c5
Soo Kang Shi <sookangshi@gmail.com>
2026-05-22 03:12:14 +0800

    Updated on 'models' package.

--- src/models/ParkingSlot.java
+++ src/models/ParkingSlot.java
@@ -1,3 +1,67 @@
+package models;
+
+
 public class ParkingSlot {
+    private String slotId;
+    private boolean isOccupied;
+    private int distanceToGate;
+    private Vehicle parkedVehicle;
+
+
+    public ParkingSlot(String slotId, int distanceToGate) {
+        this.slotId = slotId;
+        this.distanceToGate = distanceToGate;
+        this.isOccupied = false;
+        this.parkedVehicle = null;
+    }
+
+
+    public String getSlotId() {
+        return slotId;
+    }
+
+
+    public void setSlotId(String slotId) {
+        this.slotId = slotId;
+    }
+
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+
+    public void setOccupied(boolean occupied) {
+        isOccupied = occupied;
+    }
+
+
+    public int getDistanceToGate() {
+        return distanceToGate;
+    }
+
+
+    public void setDistanceToGate(int distanceToGate) {
+        this.distanceToGate = distanceToGate;
+    }
+
+
+    public Vehicle getParkedVehicle() {
+        return parkedVehicle;
+    }
+
+
+    public void setParkedVehicle(Vehicle parkedVehicle) {
+        this.parkedVehicle = parkedVehicle;
+        this.isOccupied = (parkedVehicle != null);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Slot ID: " + slotId + " | Distance to Gate: " + distanceToGate +
+              " | Occupied: " + isOccupied +  (isOccupied ? " | Vehicle: " + parkedVehicle.getLicensePlate() : "");
+    }
+
 
 }
--- src/models/Vehicle.java
+++ src/models/Vehicle.java
@@ -1,3 +1,69 @@
+package models;
+
 public class Vehicle {
 
+
+    private String licensePlate;
+    private String ownerName;
+    private long entryTime;
+    private String assignedSlotId;
+
+
+    public Vehicle(String licensePlate, String ownerName, long entryTime) {
+        this.licensePlate = licensePlate;
+        this.ownerName = ownerName;
+        this.entryTime = entryTime;
+    }
+
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+
+    public long getEntryTime() {
+        return entryTime;
+    }
+
+
+    public void setEntryTime(long entryTime) {
+        this.entryTime = entryTime;
+    }
+
+
+    public String getAssignedSlotId() {
+        return assignedSlotId;
+    }
+
+
+    public void setAssignedSlotId(String assignedSlotId) {
+        this.assignedSlotId = assignedSlotId;
+    }
+
+
+    public String toString() {
+        return "Vehicle: " + licensePlate + " | Owner: " + ownerName + " | Entry Time: " + entryTime
+                + " | Assigned Slot: " + assignedSlotId;
+    }
+
+
+   
+   
 }
+
+
